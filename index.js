1f9019891bfae35a326035acd5149d1d21882858
Giang Nguyen <giangtruongnguyen02082003@gmail.com>
2024-08-02 16:37:06 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+app.use(express.static('public'))
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Trang chủ",
+    message: "Xin chào các bạn",
+  });
+});
+
+app.get("/products", (req, res) => {
+  res.send("<h1>Trang danh sách sản phẩm</h1>");
+});
+
+app.get("/blog", (req, res) => {
+  res.send("<h1>Trang danh sách bài viết</h1>");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", {
+    title: "Trang liên hệ",
+    message: "Xin chào các bạn",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
