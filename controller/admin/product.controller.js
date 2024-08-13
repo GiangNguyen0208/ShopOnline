7efd24e470549b769f51f9b91946a1b13f45ceae
Trương Nguyễn Hương Giang <143255970+GiangNguyen0208@users.noreply.github.com>
2024-08-13 12:08:57 +0700

    Show Products (admin page)

--- controller/admin/product.controller.js
+++ controller/admin/product.controller.js
@@ -1,5 +1,13 @@
-module.exports.index = (req, res) => {
+const Product = require("../../models/product.model");
+
+// [GET] /admin/products
+module.exports.index = async (req, res) => {
+  const products = await Product.find({
+    deleted: false  // Lấy ra các Product chưa bị xóa
+  })
+  
   res.render("admin/pages/products/index", {
-    pageTitle: "Trang quản lý sản phẩm"
+    pageTitle: "Trang quản lý sản phẩm",
+    products: products
 });
 }
--- controller/client/contact.controller.js
+++ controller/client/contact.controller.js
@@ -1,6 +1,6 @@
+// [GET] /contact
 module.exports.index = (req, res) => {
     res.render("client/pages/contact/index", {
         pageTitle: "Trang liên hệ"
     })
-
 }
