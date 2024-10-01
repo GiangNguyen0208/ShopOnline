d27e439e3a18192f203064c587f918d8c11f235a
Trương Nguyễn Hương Giang <143255970+GiangNguyen0208@users.noreply.github.com>
2024-10-01 14:27:01 +0700

    initial middleware feature

--- routes/admin/dist/stock.route.dev.js
+++ routes/admin/dist/stock.route.dev.js
@@ -6,22 +6,11 @@ var router = express.Router();
 
 var multer = require('multer');
 
-var cloudinary = require("cloudinary").v2;
-
-var streamifier = require("streamifier");
-
 var path = require('path');
 
-var fs = require('fs'); // const storageMulterHelper = require("../../helper/storageMulter");
-// Set up Cloudinary
+var fs = require('fs');
 
-
-cloudinary.config({
-  cloud_name: 'dnfakbsmn',
-  api_key: '137735426286615',
-  api_secret: 'Y_-ANlwtKfN9EC7Rk_jTFR-bsCE' // Click 'View API Keys' above to copy your API secret
-
-}); // End Cloudinary
+var uploadCloud = require("../../middleware/admin/uploadCloud.middleware");
 
 var upload = multer();
 
@@ -37,47 +26,5 @@ router.patch("/change-multi", controller.changeMulti);
 router.patch("/postSale/:id", controller.postSale);
 router["delete"]("/delete/:id", controller["delete"]);
 router.get("/create", controller.create);
-router.post("/create-post", upload.single('thumbnail'), function (req, res, next) {
-  if (req.file) {
-    var _upload = function _upload(req) {
-      var result;
-      return regeneratorRuntime.async(function _upload$(_context) {
-        while (1) {
-          switch (_context.prev = _context.next) {
-            case 0:
-              _context.next = 2;
-              return regeneratorRuntime.awrap(streamUpload(req));
-
-            case 2:
-              result = _context.sent;
-              console.log(result.secure_url);
-              req.body[req.file.fieldname] = result.secure_url;
-              next();
-
-            case 6:
-            case "end":
-              return _context.stop();
-          }
-        }
-      });
-    };
-
-    var streamUpload = function streamUpload(req) {
-      return new Promise(function (resolve, reject) {
-        var stream = cloudinary.uploader.upload_stream(function (error, result) {
-          if (result) {
-            resolve(result);
-          } else {
-            reject(error);
-          }
-        });
-        streamifier.createReadStream(req.file.buffer).pipe(stream);
-      });
-    };
-
-    _upload(req);
-  } else {
-    next();
-  }
-}, validate.createPost, controller.createPost);
+router.post("/create-post", upload.single('thumbnail'), uploadCloud.upload, validate.createPost, controller.createPost);
 module.exports = router;
--- routes/admin/stock.route.js
+++ routes/admin/stock.route.js
@@ -1,20 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const multer = require('multer');
-const cloudinary = require("cloudinary").v2;
-const streamifier = require("streamifier");
 const path = require('path');
 const fs = require('fs');
 
-// const storageMulterHelper = require("../../helper/storageMulter");
-
-// Set up Cloudinary
-cloudinary.config({ 
-    cloud_name: 'dnfakbsmn', 
-    api_key: '137735426286615', 
-    api_secret: 'Y_-ANlwtKfN9EC7Rk_jTFR-bsCE' // Click 'View API Keys' above to copy your API secret
-});
-// End Cloudinary
+const uploadCloud = require("../../middleware/admin/uploadCloud.middleware");
 const upload = multer();
 
 const controller = require("../../controller/admin/stock.controller");
@@ -29,35 +19,7 @@ router.get("/create", controller.create);
 router.post(
     "/create-post", 
     upload.single('thumbnail'),
-    function (req, res, next) {
-        if (req.file) {
-            let streamUpload = (req) => {
-                return new Promise((resolve, reject) => {
-                    let stream = cloudinary.uploader.upload_stream(
-                        (error, result) => {
-                            if (result) {
-                                resolve(result);
-                            } else {
-                                reject(error);
-                            }
-                        }
-                    );
-    
-                    streamifier.createReadStream(req.file.buffer).pipe(stream);
-                });
-            };
-    
-            async function upload(req) {
-                let result = await streamUpload(req);
-                console.log(result.secure_url);
-                req.body[req.file.fieldname] = result.secure_url;
-                next();
-            }
-            upload(req);
-        } else {
-            next();
-        }
-    },
+    uploadCloud.upload,
     validate.createPost,
     controller.createPost
 );
